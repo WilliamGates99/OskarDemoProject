4af2190079b953bcd1d5b45ce1df1948304c3203
William Gates <william.gates.3299@gmail.com>
2024-03-30 19:47:51 +0330

    Implement Auth Logic

--- app/src/main/java/com/xeniac/oskardemoproject/core/di/RepositoryModule.kt
+++ app/src/main/java/com/xeniac/oskardemoproject/core/di/RepositoryModule.kt
@@ -1,11 +1,20 @@
 package com.xeniac.oskardemoproject.core.di
 
+import com.xeniac.oskardemoproject.feature_auth.data.remote.AuthRepository
+import com.xeniac.oskardemoproject.feature_auth.data.remote.AuthRepositoryImpl
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
 
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RepositoryModule {
 
+    @Binds
+    @ViewModelScoped
+    abstract fun bindAuthRepository(
+        authRepositoryImpl: AuthRepositoryImpl
+    ): AuthRepository
 }
